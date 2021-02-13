c270c519e44cee4d1244d580fd1133b5cf5582df
Génesis Zamora Paniagua <genesis.zamora@hotmail.com>
2021-02-13 10:44:02 -0600

    small changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,50 @@
 $(document).ready(function () {
 
-    $(".btn-search").on("click", function (e) {
-      getContent();
-    });
-  
-    $('input[type="text"]').keypress(function(event){
-      keycode = (event.keyCode ? event.keyCode : event.which);
-      if(keycode == '13'){
-          getContent();  
-      }
+  $(".btn-search").on("click", function (e) {
+    getContent();
   });
-  
-  });
-  
-  /**
-     * Makes a request to the API for searching an specific email address.
-     * If there's a response, it gets stored in the local storage and redirects to results page
-     */
-      function getContent(){
-      localStorage.clear(); //Clears storage for next request
-      email = $('input[type="text"]').val().toLowerCase();
-  
-      if (validateEmail(email)) {
-        const proxyurl = "https://cors-anywhere.herokuapp.com/"; //Proxy that helps enable CORS requests
-        const url =
-          "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email;
-        fetch(proxyurl + url)
-          .then((response) => response.text())
-          .then(function (contents) {
-            localStorage.setItem("userData", contents);
-            window.location.href = "layout.html";
-          })
-          .catch((e) => console.log(e));
-      } else {
-        $('input[type="text"]').addClass("error");
-        $("span.floating-label").addClass("error");
-        $(".floating-label").text("Please add a valid email address");
-      }
+
+  $('input[type="text"]').keypress(function(event){
+    keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        getContent();  
     }
-  
-  // Validates user's email request using regular expressions
-  function validateEmail(email) {
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
-      return true;
+});
+
+});
+
+/**
+   * Makes a request to ltv API to search an specific email address.
+   * If there's a response, it gets stored in the local storage and redirects to results page
+   */
+    function getContent(){
+    localStorage.clear(); //Clears storage for next request
+    email = $('input[type="text"]').val().toLowerCase();
+
+    if (validateEmail(email)) {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/"; //Proxy that helps enable CORS requests
+      const url =
+        "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email;
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          localStorage.setItem("userObject", contents);
+          window.location.href = "layout.html";
+        })
+        .catch((e) => console.log(e));
     } else {
-      return false;
+      $('input[type="text"]').addClass("error");
+      $("span.floating-label").addClass("error");
+      $(".floating-label").text("Please add a valid email address");
     }
   }
+
+// Validates user's email request using regular expressions
+function validateEmail(email) {
+  regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (email.match(regEx)) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- assets/js/outcome.js
+++ assets/js/outcome.js
@@ -1,43 +1,43 @@
 $(document).ready(function () {
-    setOutcome();
-  });
-  
-  /**
-   * Gets an object and sets its content into the result card in the result page
-   * If there's no content in the JSON object, makes sure to tell the user
-   */
-  function setOutcome() {
-    if (window.localStorage && localStorage.userData) {
-      userObject = localStorage.getItem("userData");
-      retreivedObject = JSON.parse(userObject); //parses the retreived object into an JSON object
-      if (JSON.stringify(retreivedObject) == "[]") {
-        $(".result-desc").text(
-          "Oops!  We can't seem to find the email you're looking for. Want to try again?"
+  setContent();
+});
+
+/**
+ * Gets an object and sets its content into the result card in the result page
+ * If there's no content in the JSON object, makes sure to tell the user
+ */
+function setContent() {
+  if (window.localStorage && localStorage.userObject) {
+    userObject = localStorage.getItem("userObject");
+    retreivedObject = JSON.parse(userObject); //parses the retreived object into an JSON object
+    if (JSON.stringify(retreivedObject) == "[]") {
+      $(".result-desc").text(
+        "Oops!  We can't seem to find the email you're looking for. Want to try again?"
+      );
+    } else {
+      $(".username").append(
+        retreivedObject.first_name + " " + retreivedObject.last_name
+      );
+      $(".user-description").append(retreivedObject.description);
+      $(".address").append("<p>" + retreivedObject.address + "</p>");
+      $(".email").append("<p>" + retreivedObject.email + "</p>");
+
+      for (const phone_number in retreivedObject.phone_numbers) {
+        phone = retreivedObject.phone_numbers[phone_number]
+        formatted_phone = "("+phone.substring(0, 3)+") "+phone.substring(3, 6)+"-"+phone.substring(6, 10);
+        
+        $(".phone-num").append(
+          "<p>" + `${formatted_phone}` + "</p>"
         );
-      } else {
-        $(".username").append(
-          retreivedObject.first_name + " " + retreivedObject.last_name
-        );
-        $(".user-description").append(retreivedObject.description);
-        $(".address").append("<p>" + retreivedObject.address + "</p>");
-        $(".email").append("<p>" + retreivedObject.email + "</p>");
-  
-        for (const phone_number in retreivedObject.phone_numbers) {
-          phone = retreivedObject.phone_numbers[phone_number]
-          formatted_phone = "("+phone.substring(0, 3)+") "+phone.substring(3, 6)+"-"+phone.substring(6, 10);
-          
-          $(".phone-num").append(
-            "<p>" + `${formatted_phone}` + "</p>"
-          );
-        }
-  
-        for (const relative in retreivedObject.relatives) {
-          $(".relatives").append(
-            "<p>" + `${retreivedObject.relatives[relative]}` + "</p>"
-          );
-        }
-  
-        $(".result-card").show();
       }
+
+      for (const relative in retreivedObject.relatives) {
+        $(".relatives").append(
+          "<p>" + `${retreivedObject.relatives[relative]}` + "</p>"
+        );
+      }
+
+      $(".result-card").show();
     }
   }
+}
